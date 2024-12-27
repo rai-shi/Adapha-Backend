5cfd8b0468a7c721f4ce49dede308666044f3b54
Aysenur <aysenurtak1@gmail.com>
2024-12-27 13:22:57 +0300

    team routes are added to server.ts

---
+++ src/modules/team/team.route.ts
@@ -0,0 +1,16 @@
+import { FastifyInstance, FastifyRequest } from "fastify";
+import { FastifyZodOpenApiTypeProvider } from "fastify-zod-openapi";
+
+
+
+export async function teamRoutes(server: FastifyInstance){
+    
+}
+
+export async function turkishTeamRoutes(server: FastifyInstance){
+    
+}
+
+export async function englishTeamRoutes(server: FastifyInstance){
+    
+}
